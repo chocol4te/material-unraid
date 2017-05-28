a2f716168486b1a52ba5cdfa3504964456999c6b
Ferdia McKeogh <ferdiamckeogh@outlook.com>
2017-05-28 16:15:20 +0100

    Added compression plugin

--- webpack.config.js
+++ webpack.config.js
@@ -28,12 +28,13 @@ module.exports = {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin()
+    new CompressionPlugin({
+			asset: "[path].gz[query]",
+			algorithm: "gzip",
+			test: /\.(js|html)$/,
+			threshold: 10240,
+			minRatio: 0.8
+		})
   })
   ],
 };
-
-app.get('*.js', function (req, res, next) {
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-  next();
-});
