c415dd48e9804b1b89ee78a3fffcaacc3e001c4f
Ferdia McKeogh <ferdiamckeogh@outlook.com>
2017-05-28 14:07:56 +0100

    Added empty dialogs for feedback and system info

--- src/AppBar.js
+++ src/AppBar.js
@@ -6,6 +6,10 @@ import muiThemeable from 'material-ui/styles/muiThemeable';
 import compose from 'recompose/compose';
 import { toggleSidebar as toggleSidebarAction } from 'admin-on-rest';
 import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 import ActionPowerSettingsNew from 'react-material-icons/icons/action/power-settings-new';
 import ActionReceipt from 'react-material-icons/icons/action/receipt';
@@ -13,9 +17,90 @@ import ActionHelp from 'react-material-icons/icons/action/help';
 import ActionInfo from 'react-material-icons/icons/action/info';
 import ActionFeedback from 'react-material-icons/icons/action/feedback';
 
+class InfoDialog extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Close"
+        onTouchTap={this.handleClose}
+      />
+    ];
+
+    return (
+        <span>
+        <IconButton iconStyle={iconStyle} hoveredStyle={hoveredStyle} onTouchTap={this.handleOpen}>
+            <ActionInfo/>
+        </IconButton>
+        <Dialog
+          title="System Information"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          System info should appear here.
+        </Dialog>
+        </span>
+    );
+  }
+}
+
+class FeedbackDialog extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Close"
+        onTouchTap={this.handleClose}
+      />
+    ];
+
+    return (
+        <span>
+        <IconButton iconStyle={iconStyle} hoveredStyle={hoveredStyle} onTouchTap={this.handleOpen}>
+            <ActionFeedback/>
+        </IconButton>
+        <Dialog
+          title="Feedback"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          Form for submitting feedback here.
+        </Dialog>
+        </span>
+    );
+  }
+}
+
+
 const iconStyle = {
     backgroundColor: 'transparent',
-    color: 'white'
+    color: 'white',
 };
 
 const hoveredStyle = {
@@ -24,11 +109,27 @@ const hoveredStyle = {
 
 const rightButtons = (
     <div>
-        <IconButton iconStyle={iconStyle} hoveredStyle={hoveredStyle}><ActionFeedback/></IconButton>
+        <FeedbackDialog/>
+        
         <IconButton iconStyle={iconStyle} hoveredStyle={hoveredStyle}><ActionHelp/></IconButton>
-        <IconButton iconStyle={iconStyle} hoveredStyle={hoveredStyle}><ActionInfo/></IconButton>
+        
+        <InfoDialog/>
+        
         <IconButton iconStyle={iconStyle} hoveredStyle={hoveredStyle}><ActionReceipt/></IconButton>
-        <IconButton iconStyle={iconStyle} hoveredStyle={hoveredStyle}><ActionPowerSettingsNew/></IconButton>
+        
+        <IconMenu
+            iconButtonElement={
+                <IconButton iconStyle={iconStyle} hoveredStyle={hoveredStyle}> <ActionPowerSettingsNew/> </IconButton>
+            }
+            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        >
+            <MenuItem primaryText="Start Array" />
+            <MenuItem primaryText="Stop Array" />
+            <MenuItem primaryText="Restart" />
+            <MenuItem primaryText="Shutdown" />
+            <MenuItem primaryText="Logout" />
+        </IconMenu>
     </div>
 );
 
--- src/Layout.js
+++ src/Layout.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import CircularProgress from 'material-ui/CircularProgress';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import {
     AdminRoutes,
     Sidebar,
