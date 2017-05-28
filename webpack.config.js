1d22c9e393bcd9c086827b5b05cd7e29ba71f62a
Ferdia McKeogh <ferdiamckeogh@outlook.com>
2017-05-28 16:40:48 +0100

    Fixed more syntax errors

--- webpack.config.js
+++ webpack.config.js
@@ -25,17 +25,20 @@ module.exports = {
     new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
-    }})
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin()
+    }}),
+    //new webpack.optimize.DedupePlugin(),
+    //new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
     new CompressionPlugin({
 			asset: "[path].gz[query]",
 			algorithm: "gzip",
 			test: /\.js$|\.css$|\.html$/,
 			threshold: 10240,
 			minRatio: 0.8
-		})
-  })
+    })
   ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './public'
+  }
 };
