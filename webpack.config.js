2605cf2b7d0e5a161e0c3a6fb49c3b513a1bd7b3
Ferdia McKeogh <ferdiamckeogh@outlook.com>
2017-05-28 18:38:50 +0100

    Webpack now working

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,38 @@
-path = require('path');
-const webpack = require('webpack');
+const path = require('path');
 
-var CompressionPlugin = require("compression-webpack-plugin");
+var webpack = require('webpack');
 
 module.exports = {
-
-  devtool: 'source-map',
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/',
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
-      {
-        exclude: /node_modules/,
-        loaders: ['babel-loader']
-      }
-    ],
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ['style-loader', 'css-loader'], exclude: /node_modules/ },
+    ]
   },
-
+  
   plugins: [
     new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
     }}),
-    //new webpack.optimize.DedupePlugin(),
-    //new webpack.optimize.UglifyJsPlugin(),
+    /*
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
-    new CompressionPlugin({
-			asset: "[path].gz[query]",
-			algorithm: "gzip",
-			test: /\.js$|\.css$|\.html$/,
-			threshold: 10240,
-			minRatio: 0.8
+    new webpack.CompressionPlugin({
+                        asset: "[path].gz[query]",
+                        algorithm: "gzip",
+                        test: /\.js$|\.css$|\.html$/,
+                        threshold: 10240,
+                        minRatio: 0.8
     })
+    */
   ],
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './public'
-  }
-};
+  
+}
